34c7241da2406dbc6889d6775db12050c6d5c03e
pongsatorn manusopit <ton11797@hotmail.com>
2017-11-29 23:11:55 +0700

    UI

--- src/game.java
+++ src/game.java
@@ -8,21 +8,26 @@ import org.jgrapht.graph.SimpleGraph;
 
 import java.util.*;
 
-public class game {
+public class game 
+{
     private Graph<String, DefaultEdge> G;
     private SimpleGraph<String, DefaultEdge> SG;
     private DijkstraShortestPath<String, DefaultEdge> DSP;
-    private HashSet<String> HS;
+    private HashMap<String,Integer> HM;
     private String initialState;
     private String[] change;
     private String ans;
     private int n;
-    game(int N,String I){
+    private ArrayList<Integer> light;
+    
+    public game(int N,String I)
+    {
         n=N;
         initialState=I;
-        HS = new HashSet<>();
+        HM = new HashMap<String,Integer>();
         SG = new SimpleGraph<String, DefaultEdge>(DefaultEdge.class);
         G  = (Graph<String, DefaultEdge>)SG;
+        printTable(I);
         ans();
         genChange();
         createGraph();
@@ -30,40 +35,54 @@ public class game {
     }
     public void printDefaultEdges(Collection<DefaultEdge> E, boolean f)
     {
+        int i=0;
+        String status;
         for (DefaultEdge e : E)
         {
-                System.out.printf("%18s - %18s \n", G.getEdgeSource(e),G.getEdgeTarget(e));
+            char[] TargetArray =G.getEdgeTarget(e).toCharArray();
+            if(TargetArray[HM.get(G.getEdgeTarget(e))]=='1') status="on";
+            else status="off";
+            System.out.printf("\n>>> Move %d : turn %s row %d col %d\n\n",i+1,status,HM.get(G.getEdgeTarget(e))/n+1,HM.get(G.getEdgeTarget(e))%n+1);
+            printTable(G.getEdgeTarget(e));
+            System.out.println();
+            for(int j=0;j<n;j++) System.out.printf("-----------");
+            System.out.println("------------");
+            i++;
         }
     }
 
-    public void show(){
+    public void show()
+    {
         if (G.containsVertex(initialState) && G.containsVertex(ans))
         {
             DSP = new DijkstraShortestPath<String, DefaultEdge>(SG, initialState, ans);
             List<DefaultEdge> path = DSP.getPathEdgeList();
             if (path != null)
             {
-                System.out.printf("\nShortest path from %s to %s (length = %.0f) \n",
-                        initialState, ans, DSP.getPathLength());
+                System.out.printf("\n%.0f moves to turn off all lights\n",DSP.getPathLength());
+                System.out.println();
+                for(int j=0;j<n;j++) System.out.printf("-----------");
+                System.out.println("------------");
                 printDefaultEdges(path, false);
             }
-            else
-                System.out.printf("\nNo path from %s to %s \n", initialState, ans);
+            else System.out.printf("\nImpossible to turn off all lights\n");
         }
     }
-    private void ans(){
+    private void ans()
+    {
         char[] a = new char[n*n];
         for(int i=0;i<n*n;i++)a[i]='0';
         ans = new String(a);
     }
-    private void genChange(){
-        change = new String[(n*n)];
-        for(int k=0;k<n*n;k++) {
-            int[][] table = new int[n ][n ];
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    table[i][j] = 0;
-                }
+    private void genChange()
+    {
+        change = new String[n*n];
+        for(int k=0;k<n*n;k++) 
+        {
+            int[][] table = new int[n][n];
+            for (int i=0;i<n;i++) 
+            {
+                for (int j=0;j<n;j++) table[i][j]=0;
             }
             int ii=k/n;
             int jj=k%n;
@@ -73,53 +92,90 @@ public class game {
             if(ii<n-1)table[ii+1][jj]=1;
             if(jj<n-1)table[ii][jj+1]=1;
             change[k]="";
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
+            for (int i = 0; i < n; i++) 
+            {
+                for (int j = 0; j < n; j++) 
+                {
                     change[k] = change[k].concat(Integer.toString(table[i][j]));
                 }
             }
         }
     }
-    private String changeState(String state,String change){
+    private String changeState(String state,String change)
+    {
         char[] stateArray =state.toCharArray();
         char[] changeArray =change.toCharArray();
         String newState;
-        for(int i=0;i<n*n;i++){
-            if(changeArray[i]=='1'){
-                if(stateArray[i]=='0'){
-                    stateArray[i]='1';
-                }else{
-                    stateArray[i]='0';
-                }
+        for(int i=0;i<n*n;i++)
+        {
+            if(changeArray[i]=='1')
+            {
+                if(stateArray[i]=='0') stateArray[i]='1';
+                else stateArray[i]='0';
             }
         }
         newState = new String(stateArray);
         return newState;
     }
-    private void createGraph(){
-        HS.add(initialState);
-        ArrayList<String> allTable = new ArrayList<>();
+    private void createGraph()
+    {
+        HM.put(initialState,0);
+        ArrayList<String> allTable = new ArrayList<String>();
         allTable.add(initialState);
         G.addVertex(initialState);
         boolean found=false;
-        for(int head=0;head<allTable.size() && found!=true;head++) {
+        for(int head=0;head<allTable.size() && found!=true;head++) 
+        {
             String currentState = allTable.get(head);
-            for (int i = 0; i < n * n; i++) {
+            for (int i = 0; i < n * n; i++) 
+            {
                 String newState = changeState(currentState, change[i]);
-                if (HS.contains(newState)) {
-                } else {
-                    HS.add(newState);
+                if (!HM.containsKey(newState)) 
+                {
+                    HM.put(newState,i);
                     allTable.add(newState);
                     G.addVertex(newState);
                     G.addEdge(currentState,newState);
-                    if(newState.equals(ans)){
-                        System.out.printf("F");
+                    if(newState.equals(ans))
+                    {
                         found=true;
                         break;
                     }
                 }
             }
         }
-        System.out.print(HS.size());
+        if(found==false){
+            System.out.println("!!!!!!!!! No Solution !!!!!!!!!!");
+        }
+    }
+    public void printTable(String table)
+    {
+        char[] s=table.toCharArray();
+        System.out.printf("      ");
+        for(int i=0;i<n;i++)System.out.printf("   COL %d  ",i+1);
+        System.out.println();
+        for(int i=0;i<n*n;i++)
+        {
+            if(i%n==0)
+            {
+                System.out.printf("      ");
+                for(int j=0;j<n;j++) System.out.printf("+---------");
+                System.out.println("+");
+                System.out.printf("      ");
+                for(int j=0;j<n;j++) System.out.printf("|         ");
+                System.out.printf("|\nROW %d ",i/n+1);
+            }
+            System.out.printf("|    %s    ",s[i]);
+            if(i%n==n-1)
+            {
+                System.out.println("|");
+                System.out.printf("      ");
+                for(int j=0;j<n;j++) System.out.printf("|         ");
+                System.out.println("|");
+            }
+        }
+        System.out.printf("      ");
+        for(int j=0;j<n;j++) System.out.printf("+---------");
+        System.out.println("+");
     }
 }
--- src/lightOut.java
+++ src/lightOut.java
@@ -1,15 +1,37 @@
-public class lightOut{
-    public static void main(String[] arg){
-
-//        game play = new game(2,"1010");
-//        game play1 = new game(3,"110010111");
-//        game play2 = new game(4,"0100111001000000");
-//        game play3 = new game(4,"0101110101010000");
-//        game play4 = new game(4,"0101110101110111");
-//        game play5 = new game(4,"0101100110010011");
-//        game play6 = new game(4,"1001000110010011");
-
-        game play8 = new game(6,"100000110100101110010101110111000011");
+import java.util.*;
+class lightOut
+{
+    public static void main(String[] arg)
+    {
+        //game play1 = new game(3,"110010111");
+        Scanner sc=new Scanner(System.in);
+        String con="Y",con2="Y";
+        do
+        {
+            con2="Y";
+            System.out.printf("Enter Number of rows = ");
+            int len=sc.nextInt();
+            System.out.printf("Grid size = %d x %d = %d\n",len,len,len*len);
+            if(len==4)System.out.printf("!!Warning Grid size 4x4 only 1/16 state have solution\r\n");
+            if(len>4) {
+                System.out.printf("!!Warning Grid size %dx%d some state may use very long time to solved\r\n", len, len);
+                System.out.printf("Do you want to continue(Y/N)?");
+                con2 = sc.next();
+            }
+            if(con2.equals("Y")) {
+                System.out.printf("Enter initial states (%d bits) = ", len * len);
+                String str = sc.next();
+                while (str.length() != len * len) {
+                    System.out.println("String wrong.Please try again.\n");
+                    System.out.printf("Enter initial states (%d bits) = ", len * len);
+                    str = sc.next();
+                }
+                game play = new game(len, str);
+                System.out.printf("Do you want to continue(Y/N)?");
+                con = sc.next();
+            }
+        }
+        while(con.toLowerCase().equals("y"));
     }
 
 }
